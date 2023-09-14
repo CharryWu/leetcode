98ca8c46cd7d7ebdc2b331e43fccb6d72a2d37af
Charry Wu <wuzc.charry2013@gmail.com>
2023-09-14 01:44:46 -0700

    fix DFS

--- 2024/imc/2812. Find the Safest Path in a Grid.py
+++ 2024/imc/2812. Find the Safest Path in a Grid.py
@@ -4,20 +4,37 @@ class Solution:
     def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
         bfs_queue = deque([])
         m, n = len(grid), len(grid[0])
+        startRow, startCol = 0, 0
+        endRow, endCol = m-1, n-1
 
-        def is_safeval_valid(grid, visited, i, j, safeval):
-            if i < 0 or j < 0 or i >= m or j >= n or visited[i][j]:
+        def is_safeval_valid(grid, starti, startj, dist):
+            visited = [[False] * n for i in range(m)]
+            queue = deque([(starti, startj)])
+            if grid[starti][startj] < dist:
                 return False
-            if i == m-1 and j == n-1:
-                return grid[m-1][n-1] >= safeval
+            visited[0][0] = True
 
-            visited[i][j] = True
-
-            for dx, dy in DIR:
-                nx, ny = i + dx, j + dy
-                if grid[i][j] >= safeval and is_safeval_valid(grid, visited, nx, ny, safeval):
+            while queue:
+                i, j = queue.popleft()
+                if i == endRow and j == endCol:
                     return True
+                for dx, dy in DIR:
+                    nx, ny = dx + i, dy + j
+                    if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
+                        if grid[nx][ny] < dist:
+                            continue
+                        visited[nx][ny] = True
+                        queue.append((nx, ny))
             return False
+
+            #### DFS
+            # visited[i][j] = True
+
+            # for dx, dy in DIR:
+            #     nx, ny = i + dx, j + dy
+            #     if grid[i][j] >= safeval and is_safeval_valid(grid, visited, nx, ny, safeval):
+            #         return True
+            # return False
 
         # BFS on 2D board to mark each cell's distance from nearest thieve
         for i in range(m):
@@ -37,11 +54,10 @@ class Solution:
                         grid[nx][ny] = min(grid[x][y]+1, grid[nx][ny])
 
         ans = 0 # actual safeness factor, it's not known beforehand, we need to guess it, and verify if the guess is correct
-        l, r = 1, 400 # min possible safeval is 0 (dist 0 from thieve), the one-off val is 1, max is 400 which is entire grid
+        l, r = 1, min(grid[startRow][startCol], grid[endRow][endCol]) # min possible safeval is 0 (dist 0 from thieve), the one-off val is 1, max is 400 which is entire grid
         while l <= r: # use binary search to facilitate the guess
             mid = (l+r) // 2
-            visited = [[False] * n for i in range(m)]
-            if is_safeval_valid(grid, visited, 0, 0, mid):
+            if is_safeval_valid(grid, startRow, startCol, mid):
                 ans = max(ans, mid)
                 l = mid+1
             else:
