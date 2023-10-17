1a209083699cee7f0f630289a4d52de5aaca5aab
Charry Wu <wuzc.charry2013@gmail.com>
2023-10-16 20:02:46 -0700

    Add find centers

--- 2024/ziprecruiter/codesignal.py
+++ 2024/ziprecruiter/codesignal.py
@@ -1,3 +1,7 @@
+from bisect import bisect_left
+from collections import defaultdict
+
+
 def solution1(diffs):
     num = 1500
     maxx = 1500
@@ -6,6 +10,7 @@ def solution1(diffs):
         maxx = max(num, maxx)
     return [maxx, num]
 
+
 def encode(word):
     bitmap = 0
     for ch in word:
@@ -13,6 +18,7 @@ def encode(word):
             print()
             bitmap = bitmap | (1 << (ord(ch)-ord('a')))
     return bitmap
+
 
 def solution2(word, skeletons):
     s = set(word)
@@ -28,6 +34,7 @@ def solution2(word, skeletons):
 
 def solution3(matrix):
     m, n = len(matrix), len(matrix[0])
+
     def avg_top_left(row, col):
         s = 0
         for i in range(row+1):
@@ -73,26 +80,27 @@ def solution3(matrix):
             maxx = max([tl, tr, bl, br])
             minn = min([tl, tr, bl, br])
             if maxx-minn < diff:
-                print('i=',i,'j=', j, tl, tr, bl, br)
+                print('i=', i, 'j=', j, tl, tr, bl, br)
                 resi, resj = i, j
                 diff = min(diff, maxx-minn)
 
     return [resi, resj]
 
+
 matrix = [
-    [2,1,1,2],
-    [3,2,2,3],
-    [3,2,2,3],
-    [2,1,1,2]
+    [2, 1, 1, 2],
+    [3, 2, 2, 3],
+    [3, 2, 2, 3],
+    [2, 1, 1, 2]
 ]
 
 print(solution3(matrix))
 
 matrix = [
-    [1,2,3,4,5],
-    [4,5,3,4,5],
-    [1,2,3,4,5],
-    [4,5,3,4,5],
+    [1, 2, 3, 4, 5],
+    [4, 5, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [4, 5, 3, 4, 5],
 ]
 
 print(solution3(matrix))
@@ -105,9 +113,9 @@ def solution4(operations):
         optype, a, b = operation
         if optype == 0:
             if b > a:
-                saved.append((b,a))
+                saved.append((b, a))
             else:
-                saved.append((a,b))
+                saved.append((a, b))
 
         elif optype == 1:
             ww, hh = a, b
@@ -124,6 +132,50 @@ def solution4(operations):
     return res
 
 
+def solution5(centers):
+    d = defaultdict(list)
+    for x, y in centers:
+        d[x].append(y)
+    xlist = sorted(d.keys())
 
+    for xx in xlist:
+        d[xx].sort()
+    # print(d)
+    # print(xlist)
+    res = 0
+    for xx in xlist:
+        for i in range(len(d[xx])):
+            base_y = d[xx][i]
+            for j in range(i+1, len(d[xx])):
+                yy = d[xx][j]
 
+                if abs(yy - base_y) <= 2:
+                    res += 1
+                else:
+                    break
 
+        if xx+1 in d:
+            for i in range(len(d[xx])):
+                base_y = d[xx][i]
+                startj = bisect_left(d[xx+1], base_y-2)
+                for j in range(startj, len(d[xx+1])):
+                    yy = d[xx+1][j]
+                    if abs(yy - base_y) <= 2:
+                        res += 1
+                    else:
+                        break
+
+        if xx+2 in d:
+            for i in range(len(d[xx])):
+                base_y = d[xx][i]
+                startj = bisect_left(d[xx+2], base_y-2)
+                for j in range(startj, len(d[xx+2])):
+                    yy = d[xx+2][j]
+                    if abs(yy - base_y) <= 2:
+                        # if xx == 10:
+                        #     print('match found x+2: ', yy)
+                        res += 1
+                    else:
+                        break
+
+    return res
