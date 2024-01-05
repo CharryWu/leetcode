7a824bf0ab520937d74663e69aa909ee724ac21a
Charry Wu <charry@evt.ai>
2024-01-05 01:52:00 -0800

    Add move element

--- two-pointer/slow-fast-pointer/283. Move Zeroes.py
+++ two-pointer/slow-fast-pointer/283. Move Zeroes.py
@@ -3,18 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def removeElement(nums, val):
-            fast, slow = 0, 0
-            while fast < len(nums):
-                if nums[fast] != val:
-                    nums[slow] = nums[fast]
-                    slow += 1
-            
-                fast += 1
-            
-            return slow
-    
-        p = removeElement(nums, 0)
-        while p < len(nums):
-            nums[p] = 0
-            p += 1
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                el = nums[fast]
+                # be careful of the assignment order
+                nums[fast] = 0
+                nums[slow] = el
+                slow += 1
