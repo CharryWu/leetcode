65a9713b1d5f573badea7668251058f68fe4980a
Charry Wu <wuzc.charry2013@gmail.com>
2024-06-21 17:33:08 -0700

    Add product of array except self

--- prefix-sum/238. Product of Array Except Self.py
+++ prefix-sum/238. Product of Array Except Self.py
@@ -1,27 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        
-        res = [1] * n
-        prefix = [1] * n # prefix 是不包括 i 的前缀和
-        suffix = [1] * n # suffix 是不包括 i 的后缀和
-        
-        i = 0
-        cur = 1
-        while i < n:
-            prefix[i] = cur
-            cur = cur * nums[i]
-            i += 1
-        
-        cur = 1
-        i = n-1
-        while i >= 0:
-            suffix[i] = cur
-            cur = cur * nums[i]
-            i -= 1
-        
+        leftprod = [1] * n
+        rightprod = [1] * n
+
+        for i in range(1, n):
+            leftprod[i] = leftprod[i-1] * nums[i-1]
+        for i in range(n-2, -1, -1):
+            rightprod[i] = rightprod[i+1] * nums[i+1]
+
+        res = [0] * n
         for i in range(n):
-            res[i] = prefix[i] * suffix[i]
+            res[i] = leftprod[i] * rightprod[i]
         return res
 
 """
@@ -29,22 +19,22 @@ class Solution:
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        
+
         res = [1] * n
-        
+
         i = 0
         cur = 1
         while i < n:
             res[i] = cur # 注意这句和下句的顺序不能颠倒。先写入res[i]保证res[i]存的是 i-1 的 running product
             cur = cur * nums[i]
             i += 1
-        
+
         cur = 1
         i = n-1
         while i >= 0:
             res[i] = res[i] * cur
             cur = cur * nums[i]
             i -= 1
-        
+
         return res
 """
