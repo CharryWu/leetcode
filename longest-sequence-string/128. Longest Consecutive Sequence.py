fa4449d3ceb6f1806095ee0c53c2db631438d7b3
Charry Wu <wuzc.charry2013@gmail.com>
2024-06-22 17:28:49 -0700

    Update 128 lcs

--- longest-sequence-string/128. Longest Consecutive Sequence.py
+++ longest-sequence-string/128. Longest Consecutive Sequence.py
@@ -1,20 +1,15 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        s = set()
-        for num in nums:
-            s.add(num)
-        
+        """
+        Turn original input into a set to detect if the prev number occured
+        """
+        num_set = set(nums) # turn list into set for fast query
         res = 0
-        for num in s:
-            if num-1 in s:
-                continue
-            cur_num = num
-            cur_len = 1
 
-            while cur_num + 1 in s:
-                cur_num += 1
-                cur_len += 1
-            
-            res = max(res, cur_len)
-        
+        for num in nums:
+            if num-1 not in num_set: # found start of new sequence, doesn't have prev
+                length = 1
+                while num+length in num_set: # go as far as we can to find the end of consecutive sequence
+                    length += 1
+                res = max(res, length)
         return res
