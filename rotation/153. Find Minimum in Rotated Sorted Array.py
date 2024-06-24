52f5dd3a5621e5d7c39b36929f398c8818319076
Charry Wu <wuzc.charry2013@gmail.com>
2024-06-24 13:27:51 -0700

    add sorted array

--- rotation/153. Find Minimum in Rotated Sorted Array.py
+++ rotation/153. Find Minimum in Rotated Sorted Array.py
@@ -4,8 +4,8 @@ class Solution:
         detect whether mid pointer is in left portion or right portion using:
         Condition nums[mid] >= nums[l]
 
-        if mid pointer in left portion, check left
-        if mid pointer in right portion, check right
+        if mid pointer in left portion, check right
+        if mid pointer in right portion, check left
         """
         l, r = 0, len(nums)-1
         res = nums[0]
@@ -16,8 +16,8 @@ class Solution:
                 break
             mid = (l+r) // 2
             res = min(res, nums[mid]) # update result with mid pointer
-            if nums[mid] >= nums[l]: # >= handles edge case
-                l = mid + 1
+            if nums[mid] >= nums[l]: # mid at left sorted portion
+                l = mid + 1 # search right portion
             else:
                 r = mid - 1
         return res
