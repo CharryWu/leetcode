5cca8fef8238cfc8e92f50d67bf3703a017f1d6b
Charry Wu <wuzc.charry2013@gmail.com>
2024-07-20 13:22:30 -0700

    Add subset

--- backtracking/permutation-combination-subset/90. Subsets II.py
+++ backtracking/permutation-combination-subset/90. Subsets II.py
@@ -1,18 +1,23 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        nums.sort()
         res = []
-        path = []
-        def backtrack(i):
-            res.append(list(path))
-            
-            for next_idx in range(i, n): # consider all later elements in nums
-                candidate = nums[next_idx]
-                if next_idx > i and candidate == nums[next_idx-1]:
-                    continue
-                path.append(candidate)
-                backtrack(next_idx+1) # 
-                path.pop()
-        backtrack(0)
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset.copy())
+                return
+
+            # All subset that include nums[i]
+            subset.append(nums[i])
+            backtrack(i+1, subset)
+
+            # All subset that does not include nums[i]
+            subset.pop()
+            # since array is sorted, skip all duplicates
+            while i < len(nums)-1 and nums[i] == nums[i+1]:
+                i += 1
+
+            backtrack(i+1, subset)
+        backtrack(0, [])
         return res
