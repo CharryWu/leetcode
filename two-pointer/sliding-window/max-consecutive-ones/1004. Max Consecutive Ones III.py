1cf1e7331fa21f6538690b4a6fdbc3dbb2a02c9b
Charry Wu <wuzc.charry2013@gmail.com>
2024-08-29 18:47:09 -0700

    Add 1004

--- two-pointer/sliding-window/max-consecutive-ones/1004. Max Consecutive Ones III.py
+++ two-pointer/sliding-window/max-consecutive-ones/1004. Max Consecutive Ones III.py
@@ -1,26 +1,17 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        """
-        Translation:
-        Find the longest subarray with at most K zeros.
-        """
-        n = len(nums)
-        left, right = 0, 0
-        window_zeros = 0
         res = 0
-        while right < len(nums):
-            rnum = nums[right]
-            right += 1
-            
-            if rnum == 0:
-                window_zeros += 1
-            if window_zeros <= k:
-                res = max(res, right-left)
-            
-            while window_zeros > k:
-                lnum = nums[left]
-                left += 1
-                if lnum == 0:
-                    window_zeros -= 1
-            
+        n = len(nums)
+        zero_count = 0
+        i = 0
+        for j, num in enumerate(nums):
+            if num == 0:
+                zero_count += 1
+                # k can be zero, the sliding window could be not tolerating any zero,
+                # so the sliding window can be empty, condition check must be i <= j to allow empty window
+                while zero_count > k and i <= j:
+                    if nums[i] == 0:
+                        zero_count -= 1
+                    i += 1
+            res = max(res, j-i+1)
         return res
