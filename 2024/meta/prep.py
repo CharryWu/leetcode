dfe1daa73aceed2f481e3f5294b8af09f737023d
Charry Wu <wuzc.charry2013@gmail.com>
2024-08-31 18:29:56 -0700

    Add prep

--- 2024/meta/prep.py
+++ 2024/meta/prep.py
@@ -1,4 +1,4 @@
-# 162. Find Peak Element
+########## 162. Find Peak Element ##########
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         n = len(nums)
@@ -17,6 +17,64 @@ class Solution:
             else:
                 right = mid-1
         return -1
+
+########## 1249. Minimum Remove to Make Valid Parentheses ##########
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        """
+        Let's put all this together into a 2-pass algorithm.
+        1. Identify all indexes that should be removed. These are unmatched ")" and "(".
+        2. Build a new string with removed indexes.
+
+        Algorithm:
+        Loop thru characters in s: 1. skip non "()" characters, 2. add any "("'s index to stack,
+        3. any ")" is either matched or unmatched.
+            If ")" is matched, pop one "(" from stack,
+            If ")" is unmatched, we immediately know it should be removed.
+
+        Finally, if we encounter any
+        """
+        stack = [] # stack of indexes of '(' or ')'
+        index_to_remove = set()
+
+        # process s to obtain stack elements
+        for i, c in enumerate(s):
+            if c not in "()":
+                continue
+            elif c == '(':
+                stack.append(i)
+            else:
+                if len(stack) == 0: # unmatched )
+                    index_to_remove.add(i)
+                else: # matched )
+                    stack.pop()
+
+        index_to_remove |= set(stack)
+        return ''.join([c if i not in index_to_remove else '' for i, c in enumerate(s) ])
+
+########## 1004. Max Consecutive Ones III ##########
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        """
+        Find the max length of sliding window that may contain up to k zeros
+        """
+        res = 0
+        zero_count = 0
+        i = 0
+        for j, num in enumerate(nums):
+            # NO NEED TO CHECK num == 1 since it doesn't affect window size
+            # if num == 1:
+            #     pass # don't use continue here, we still want to update `res`!
+            # elif num == 0:
+            if num == 0:
+                zero_count += 1
+                while zero_count > k and i <= j:
+                    if nums[i] == 0:
+                        zero_count -= 1
+                    i += 1
+
+            res = max(res, j-i+1)
+        return res
 
 ########## 314. Binary Tree Vertical Order Traversal ##########
 # Definition for a binary tree node.
@@ -48,3 +106,41 @@ class Solution:
         for col in sorted(list(columns.keys())):
             res.append(columns[col])
         return res
+
+########## 127. Word Ladder ##########
+from collections import defaultdict, deque
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        """
+        Since we're getting MINIMUM transformation needed, use BFS
+        """
+        if beginWord == endWord:
+            return 0
+        wordset = set(wordList)
+        if endWord not in wordset:
+            return 0
+        mapping = defaultdict(list) # intermediary "c*t" => actual words
+        for word in wordset:
+            for i in range(len(word)):
+                intermediate = word[:i] + '*' + word[i+1:]
+                mapping[intermediate].append(word)
+
+        visited = set([beginWord])
+        queue = deque([beginWord])
+        res = 1 # at least one transformation to reach from beginWord to endWord
+
+        while queue:
+            for i in range(len(queue)):
+                word = queue.popleft()
+                if word == endWord:
+                    return res
+                visited.add(word)
+
+                for j in range(len(word)):
+                    pattern = word[:j] + '*' + word[j+1:]
+                    for neighbor in mapping[pattern]:
+                        if neighbor not in visited:
+                            visited.add(neighbor)
+                            queue.append(neighbor)
+            res += 1
+        return 0
