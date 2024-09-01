1bb9f512b9528ba90dd0499875c23734e333c7c5
Charry Wu <wuzc.charry2013@gmail.com>
2024-08-31 18:42:28 -0700

    Add prep

--- 2024/meta/prep.py
+++ 2024/meta/prep.py
@@ -1,5 +1,9 @@
 ########## 162. Find Peak Element ##########
 class Solution:
+    """
+    O(log n) time complexity
+    O(1) space complexity
+    """
     def findPeakElement(self, nums: List[int]) -> int:
         n = len(nums)
         if n < 2 or nums[0] > nums[1]:
@@ -20,6 +24,10 @@ class Solution:
 
 ########## 1249. Minimum Remove to Make Valid Parentheses ##########
 class Solution:
+    """
+    O(n) time complexity
+    O(n) space complexity
+    """
     def minRemoveToMakeValid(self, s: str) -> str:
         """
         Let's put all this together into a 2-pass algorithm.
@@ -54,6 +62,10 @@ class Solution:
 
 ########## 1004. Max Consecutive Ones III ##########
 class Solution:
+    """
+    O(n) time complexity
+    O(1) space complexity
+    """
     def longestOnes(self, nums: List[int], k: int) -> int:
         """
         Find the max length of sliding window that may contain up to k zeros
@@ -76,6 +88,37 @@ class Solution:
             res = max(res, j-i+1)
         return res
 
+########## 528. Random Pick with Weight ##########
+import random
+class Solution:
+    """
+    Use prefix sum array + binary search to achieve log(n) time
+    O(n) time complexity to prepare prefix sum, O(log n) time complexity to search
+
+    Time O(n) | Space O(n)
+    """
+    def __init__(self, w: List[int]):
+        self.prefix = []
+        total = 0
+        for weight in w:
+            total += weight
+            self.prefix.append(total)
+        self.total = total
+
+    def pickIndex(self):
+        rand = random.random()
+        cutoff = rand * self.total
+        # find first element greater than or equal to cutoff
+        low, high = 0, len(self.prefix)
+        while low < high:
+            mid = (low + high) // 2
+            if cutoff > self.prefix[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
+
+
 ########## 314. Binary Tree Vertical Order Traversal ##########
 # Definition for a binary tree node.
 # class TreeNode:
@@ -87,6 +130,12 @@ class Solution:
 from collections import defaultdict, deque
 
 class Solution:
+    """
+    Use bfs w/ deque to achieve O(n) time complexity
+
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    """
     def verticalOrder(self, root: TreeNode) -> List[List[int]]:
         """
         Group nodes by column, while maintaining row order inside each group
@@ -106,6 +155,62 @@ class Solution:
         for col in sorted(list(columns.keys())):
             res.append(columns[col])
         return res
+
+########## 50. Pow(x, n) ##########
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        """
+        Achieve log(n) time complexity using binary exponentiation
+        """
+        # edge case: 0^n == 0, 1^n == 1, x^1 == n
+        if x == 0 or x == 1 or n == 1:
+            return x
+        if n == 0: # x^0 == 1
+            return 1
+
+        # if n is negative, we need to use 1/x instead of x, and flip the sign of n
+        if n < 0:
+            return self.myPow(1/x, -n)
+
+        # use binary exponentiation
+        if n % 2 == 1:
+            return x * self.myPow(x, n-1)
+        else:
+            res = self.myPow(x, n//2)
+            return res ** 2
+
+
+import heapq
+class MedianFinder:
+    """
+    Use min-heap to store lower half values, and max-heap to store upper half values
+    Time O(log(n)) | Space O(n)
+    """
+    def __init__(self):
+        self.minheap = [] # store upper half values
+        self.maxheap = [] # store lower half values
+
+    def addNum(self, num: int) -> None:
+        if len(self.minheap) > 0 and num > self.minheap[0]:
+            heapq.heappush(self.minheap, num)
+        else:
+            heapq.heappush(self.maxheap, -num)
+        # rebalance
+        if len(self.minheap) - len(self.maxheap) > 1:
+            upper_low = heapq.heappop(self.minheap)
+            heapq.heappush(self.maxheap, -upper_low)
+        elif len(self.maxheap) - len(self.minheap) > 1:
+            lower_high = -heapq.heappop(self.maxheap)
+            heapq.heappush(self.minheap, lower_high)
+
+    def findMedian(self) -> float:
+        m, n = len(self.minheap), len(self.maxheap)
+        if m == n:
+            return (self.minheap[0] - self.maxheap[0]) / 2
+        elif m > n:
+            return self.minheap[0]
+        else:
+            return -self.maxheap[0]
 
 ########## 127. Word Ladder ##########
 from collections import defaultdict, deque
