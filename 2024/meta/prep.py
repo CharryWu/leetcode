351ccd8ce0bd5ad35f91c05373eff5c6778a2bf8
Charry Wu <wuzc.charry2013@gmail.com>
2024-09-02 20:34:20 -0700

    Add 76

--- 2024/meta/prep.py
+++ 2024/meta/prep.py
@@ -581,3 +581,32 @@ class SparseVector:
             else:
                 j += 1
         return res
+
+############# 76. Minimum Window Substring ############
+from collections import Counter
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        m, n = len(s), len(t)
+        if n > m:
+            return ""
+        cs, ct = Counter(), Counter(t)
+        res = ""
+        i, j = 0, 0 # left, right pointer to s
+
+        def valid(window, need):
+            for c in need.keys():
+                if window[c] < need[c]:
+                    return False
+            return True
+
+        while j < m:
+            cs[s[j]] += 1
+            j += 1
+
+            while i < j and valid(cs, ct):
+                if j-i < len(res) or res == "":
+                    res = s[i:j]
+                cs[s[i]] -= 1
+                i += 1
+
+        return res
--- two-pointer/sliding-window/76. Minimum Window Substring.py
+++ two-pointer/sliding-window/76. Minimum Window Substring.py
@@ -1,34 +1,27 @@
 from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        """
-        Use sliding window, forward left pointer (shrink window) as long as condition is met
-        (to obtain minimum window), forward right pointer if condition is not met
+        m, n = len(s), len(t)
+        if n > m:
+            return ""
+        cs, ct = Counter(), Counter(t)
+        res = ""
+        i, j = 0, 0 # left, right pointer to s
 
-        Condition:
-        """
-        ns, nt = len(s), len(t)
-        countT, window = Counter(t), Counter()
+        def valid(window, need):
+            for c in need.keys():
+                if window[c] < need[c]:
+                    return False
+            return True
 
-        have, need = 0, len(countT) # unique characters in string of T
-        res, resLen = [-1, -1], float('inf')
-        l = 0
-        for r in range(ns):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
+        while j < m:
+            cs[s[j]] += 1
+            j += 1
 
-            if c in countT and window[c] == countT[c]: # check if window satisfies condition of T
-                have += 1
+            while i < j and valid(cs, ct):
+                if j-i < len(res) or res == "":
+                    res = s[i:j]
+                cs[s[i]] -= 1
+                i += 1
 
-            while have == need: # met condition, forward left pointer (shrink window) until condition is not met
-                if (r - l + 1) < resLen: # found new minimum length, update window length
-                    res = [l, r]
-                    resLen = r - l + 1
-
-                window[s[l]] -= 1 # keep shrinking
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
-                    have -= 1
-                l += 1
-        l, r = res
-        return s[l : r + 1] if resLen != float("infinity") else ""
-
+        return res
